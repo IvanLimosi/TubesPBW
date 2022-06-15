3e3b1345d14b5c9dcbd8585bf9135cce53433908
Ivan Limosi <6181801032@student.unpar.ac.id>
2022-06-15 23:18:46 +0700

    pindah css ke public

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
 
 const port = 8080;
 const app = express();
 
 app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
 
 app.listen(port, () => {
     console.log(`Server running at: http://localhost:${port}/`);
