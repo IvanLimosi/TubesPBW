6f5357df4d473f724c441b55dda6e5036a968294
Ivan Limosi <6181801032@student.unpar.ac.id>
2022-06-15 20:12:55 +0700

    edit

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ app.get('/', (req, res) => {
     res.render('home');
 });
 
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
 // app.get('/add', (req,res) => {
 //     res.render('add');
 // });
